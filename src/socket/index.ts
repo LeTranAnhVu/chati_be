4f22e49c205adaab40a550b5475ce3c99153724e
letrananhvu <letrananhvu>
2020-09-22 22:53:10 +0300

    create namespace and room for chat

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,15 +1,38 @@
 import socketio from "socket.io"
 import http from "http"
 
+type JoinRoomPayload = {
+  room: string;
+}
+type ChatPayload = {
+  room: string;
+  body: string;
+}
+
 export default function createSocket(server: http.Server) {
   const io = socketio(server)
 
-  io.on("connect", (socket) => {
-    console.log("socket connected")
+  const chatNsp = io.of("/chat")
+  // middleware
+  chatNsp.use((socket, next) => {
+    // console.log("middleware is called", socket.handshake.query)
+    next()
+  })
 
+  chatNsp.on("connection", (socket) => {
+    // console.log("client connect to /chat socket")
 
-    socket.on("message", (payload: any ) => {
-      console.log(payload)
+    socket.on("join-room", (payload: JoinRoomPayload) => {
+      const {room} = payload
+      socket.join(room)
+      console.log("socket is joined room ", room)
+    })
+
+    socket.on("client-send-message", (payload: ChatPayload) => {
+      const {room, body} = payload
+      console.log("--->", room)
+      console.log("--->", body)
+      chatNsp.in(room).emit("server-send-message", body)
     })
 
 
